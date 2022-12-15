386f2440757eacb5ec80d56a2f17e4bbf928179e
aratius <arata1129matsu@icloud.com>
2022-12-15 18:37:06 +0900

    configure member

--- Assets/Scripts/Member.cs
+++ Assets/Scripts/Member.cs
@@ -8,68 +8,81 @@ using DG.Tweening;
 /// </summary>
 public class Member : MonoBehaviour
 {
-    public int index = -999;
-    private readonly Color _BASECOLOR = Color.white;
+  public int index = -999;
+  private readonly Color _BASECOLOR = Color.white;
 
-    public void Init(int i) {
-        this.index = i;
-    }
+  public void Init(int i)
+  {
+    this.index = i;
+  }
 
-    void OnTriggerEnter2D(Collider2D other)
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    string tag = other.gameObject.tag;
+    string instruction = tag;
+
+    Color color = Color.white;
+
+    if (instruction == "leftHandUp")
     {
-        string tag = other.gameObject.tag;
-        string instruction = tag;
+      color = new Color(1f, 0f, 0f, 1f);
 
-        Color color = Color.white;
-
-        if(instruction == "leftHandUp")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else if (instruction == "leftHandDown")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else if (instruction == "rightHandUp")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else if (instruction == "rightHandDown")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else if (instruction == "standUp")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else if (instruction == "standDown")
-        {
-            color = new Color(1f, 0f, 0f, 1f);
-
-        } else {
-            Debug.LogWarning("undefined instruction");
-            return;
-        };
-
-        if(this.index >= 0)
-        {
-            this._DoColor(color);
-            OscSender.Instance.Send(this.index, $"/{instruction}", 0);
-            // TODO: 1.8sec後にLEDとmaxへOSC
-            Utils.DoItAfter(() => {
-                OscSender.Instance.Send(this.index, $"/tone", 0);
-            }, 1800);
-        }
     }
-
-    /// <summary>
-    /// do color
-    /// </summary>
-    /// <param name="color"></param>
-    private void _DoColor(Color color)
+    else if (instruction == "leftHandDown")
     {
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(this.GetComponent<SpriteRenderer>().DOColor(color, .1f));
-        sequence.Append(this.GetComponent<SpriteRenderer>().DOColor(Color.white, 3f));
+      color = new Color(1f, 0f, 0f, 1f);
+
     }
+    else if (instruction == "rightHandUp")
+    {
+      color = new Color(1f, 0f, 0f, 1f);
+
+    }
+    else if (instruction == "rightHandDown")
+    {
+      color = new Color(1f, 0f, 0f, 1f);
+
+    }
+    else if (instruction == "standUp")
+    {
+      color = new Color(1f, 0f, 0f, 1f);
+
+    }
+    else if (instruction == "standDown")
+    {
+      color = new Color(1f, 0f, 0f, 1f);
+
+    }
+    else
+    {
+      Debug.LogWarning("undefined instruction");
+      return;
+    };
+
+    if (this.index >= 0)
+    {
+      this._DoColor(color);
+      OscSender.Instance.Send(this.index, $"/{instruction}", 0);
+      // TODO: 1.8sec後にLEDとmaxへOSC
+      Utils.DoItAfter(() =>
+      {
+        OscSender.Instance.Send(this.index, $"/tone", 1);
+        Utils.DoItAfter(() =>
+        {
+          OscSender.Instance.Send(this.index, $"/tone", 0);
+        }, 100);
+      }, 2100);
+    }
+  }
+
+  /// <summary>
+  /// do color
+  /// </summary>
+  /// <param name="color"></param>
+  private void _DoColor(Color color)
+  {
+    Sequence sequence = DOTween.Sequence();
+    sequence.Append(this.GetComponent<SpriteRenderer>().DOColor(color, .1f));
+    sequence.Append(this.GetComponent<SpriteRenderer>().DOColor(Color.white, 3f));
+  }
 }
